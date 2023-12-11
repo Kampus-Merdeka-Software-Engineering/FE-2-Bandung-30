55e0fa6fec4cc8faf467ab66c90b2ddc160c5efd
Yogih Arahman <yogiarrahman0@gmail.com>
2023-12-11 10:10:40 +0700

    memperbaiki search di halaman search

--- js/search_results.js
+++ js/search_results.js
@@ -21,7 +21,7 @@ searchBox.addEventListener("keydown", function (e) {
 
     const keyword = searchBox.value.trim();
     if (keyword !== "") {
-      window.location.href = `./berita/search_results.html?keyword=${encodeURIComponent(
+      window.location.href = `../berita/search_results.html?keyword=${encodeURIComponent(
         keyword
       )}`;
     }
